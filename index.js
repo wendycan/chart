5b68e1ffe1210563dea3115de710291a4de356e3
Wendy Wang <wilxnwang@gmail.com>
2015-03-12 10:18:43 +0800

    Add nsp

--- index.js
+++ index.js
@@ -5,18 +5,27 @@ var io = require('socket.io')(http);
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+
+io.of('/chat').on('connection', function(socket){
+  console.log('message: ' + 'connected');
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
-    io.emit('chat message', msg);
-    // socket.broadcast.emit('chat message', msg);
+    io.of('chat').emit('display chat message', msg);
+    // socket.broadcast.emit('display chat message', msg);
   });
+  // socket.on('disconnect', function(){
+  //   console.log('user disconnected');
+  // });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+// io.of('/todos').on('connection', function(socket){
+//   console.log('message: ' + 'todos connected');
+//   socket.on('new todo', function(msg){
+//     console.log('message: ' + msg);
+//     io.emit('new todo message', msg);
+//   });
+// });
+
+http.listen(3001, function(){
+  console.log('listening on *:3001');
 });
