40bf012fcd4cd24d82c2ff2042f94c72077b3bc3
Wendy Wang <wilxnwang@gmail.com>
2015-03-19 16:30:53 +0800

    Add cherish sockets

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var cherish = require('./routes/cherish');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -24,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/cherish', cherish);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,5 +57,7 @@ app.use(function(err, req, res, next) {
     });
 });
 
+var express = require('express');
+var router = express.Router();
 
 module.exports = app;
---
+++ routes/cherish.js
@@ -0,0 +1,111 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Cherish' });
+});
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var request = require('request');
+var users = [];
+var usernames = [];
+
+io.set('authorization', function (handshakeData, callback) {
+  // findDatabyip is an async example function
+
+  var token = handshakeData._query.token;
+  // request('http://wendycan.org/api/v1/user?auth_token=' + token, function (error, response, body) {
+  request('http://wendycan-local.com:3000/api/v1/user?auth_token=' + token, function (error, response, body) {
+    // to save to db
+    if (JSON.parse(body).username) {
+      console.log('success');
+      callback(null, true);
+    } else {
+      console.log('failed');
+      callback(null, false);
+    }
+  });
+});
+
+io.of('/todos').on('connection', function(socket){
+  var cookie_string = socket.request.headers.cookie;
+  var sid = cookie_string.split('=')[1];
+  var user = {sid: sid};
+
+  socket.on('join chat', function (msg) {
+    console.log('todos connected');
+
+    var index = usernames.indexOf(msg);
+    if (index < 0) {
+      user.name = msg;
+      users.push(user);
+      usernames.push(msg);
+    } else {
+      usernames[index].sid = sid;
+    }
+
+    var data = {
+      currentUser: msg,
+      users: usernames
+    };
+
+    io.of('/todos').emit('join message', JSON.stringify(data));
+  });
+
+  socket.on('todo changed', function(msg){
+    var address = socket.handshake.address;
+    var data = JSON.parse(msg);
+    data.ip = address;
+
+    io.of('/todos').emit('todo message', JSON.stringify(data));
+  });
+
+  socket.on('add chart', function (msg) {
+    var address = socket.handshake.address;
+    var data = JSON.parse(msg);
+    data.ip = address;
+
+    io.of('/todos').emit('chart message', JSON.stringify(data));
+  });
+
+  socket.on('disconnect', function (data) {
+    console.log('message: leave' );
+
+    var msg;
+    for(var i in users) {
+      if (users[i].sid == sid) {
+        msg = users[i].name;
+        users.splice(i, 1);
+        usernames.splice(i, 1);
+      };
+    }
+
+    io.of('/todos').emit('leave message', msg);
+  });
+
+  socket.on('leave page', function () {
+    console.log('message: leave' );
+
+    var msg;
+    for(var i in users) {
+      if (users[i].sid == sid) {
+        msg = users[i].name;
+        users.splice(i, 1);
+        usernames.splice(i, 1);
+      };
+    }
+
+    io.of('/todos').emit('leave message', msg);
+  });
+
+});
+
+http.listen(3002, function(){
+console.log('listening on *:3002');
+});
+
+
+module.exports = router;
