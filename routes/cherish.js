694c21c8651fd4930e1b1350685620b8d645226c
Wendy Wang <wilxnwang@gmail.com>
2015-03-19 17:12:48 +0800

    Rename port to 3003

--- routes/cherish.js
+++ routes/cherish.js
@@ -18,7 +18,7 @@ io.set('authorization', function (handshakeData, callback) {
 
   var token = handshakeData._query.token;
   // request('http://wendycan.org/api/v1/user?auth_token=' + token, function (error, response, body) {
-  request('http://wendycan-local.com:3000/api/v1/user?auth_token=' + token, function (error, response, body) {
+  request('http://wendycan-local.com:3003/api/v1/user?auth_token=' + token, function (error, response, body) { //Rails use 3003!!!
     // to save to db
     if (JSON.parse(body).username) {
       console.log('success');
