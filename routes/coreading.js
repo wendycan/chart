c1ade45263cd0998270d9db47978c2fe40196c2c
wendy <wendycaner@gmail.com>
2015-05-13 07:39:48 +0800

    Fetch group and user

--- routes/coreading.js
+++ routes/coreading.js
@@ -12,6 +12,7 @@ var io = require('socket.io')(http);
 var request = require('request');
 var users = [];
 var usernames = [];
+var group_users = [];
 
 io.set('authorization', function (handshakeData, callback) {
   // findDatabyip is an async example function
@@ -22,37 +23,47 @@ io.set('authorization', function (handshakeData, callback) {
   request('http://localhost:3001/api/v1/groups/' + group_id + '?auth_token=' + token, function (error, response, body) {
   // request('http://wendycan-local.com:3003/api/v1/user?auth_token=' + token, function (error, response, body) { //Rails use 3003!!!
     // to save to db
-    if (JSON.parse(body).username) {
-      console.log('success');
+    if (JSON.parse(body)) {
+      console.log('success group');
+      group_users = JSON.parse(body)
       callback(null, true);
     } else {
       console.log('failed');
       callback(null, false);
     }
   });
+
 });
 io.of('/annotations').on('connection', function(socket){
   var cookie_string = socket.request.headers.cookie;
   var sid = cookie_string.split('=')[1];
   var user = {sid: sid};
+  var token = socket.handshake.query.token;
+  request('http://localhost:3001/api/v1/user?auth_token=' + token, function (error, response, body) {
+    if (JSON.parse(body).username) {
+      console.log('success username');
+      user.username = JSON.parse(body).username;
+      console.log(user)
 
-  socket.on('change annotation', function(msg){
-    var address = socket.handshake.address;
-    var data = JSON.parse(msg);
-    data.ip = address;
+      socket.on('change annotation', function(msg){
+        var address = socket.handshake.address;
+        var data = JSON.parse(msg);
+        data.ip = address;
 
-    io.of('/annotations').emit('change annotation', JSON.stringify(data));
+        io.of('/annotations').emit('change annotation', JSON.stringify(data));
+      });
+
+      socket.on('add chart', function (msg) {
+        var address = socket.handshake.address;
+        var data = JSON.parse(msg);
+        data.ip = address;
+
+        io.of('/annotations').emit('chart message', JSON.stringify(data));
+      });
+    }
   });
-
-  socket.on('add chart', function (msg) {
-    var address = socket.handshake.address;
-    var data = JSON.parse(msg);
-    data.ip = address;
-
-    io.of('/annotations').emit('chart message', JSON.stringify(data));
-  });
-
 });
+
 http.listen(3003, function(){
 console.log('listening on *:3003');
 });
