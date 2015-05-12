db1606cb929c34b284d21a96ff2cefa8ea616db3
wendy <wendycaner@gmail.com>
2015-05-12 20:21:17 +0800

    Get group data

--- routes/coreading.js
+++ routes/coreading.js
@@ -17,7 +17,9 @@ io.set('authorization', function (handshakeData, callback) {
   // findDatabyip is an async example function
 
   var token = handshakeData._query.token;
-  request('http://localhost:3003/api/v1/user?auth_token=' + token, function (error, response, body) {
+  var group_id = handshakeData._query.group;
+
+  request('http://localhost:3001/api/v1/groups/' + group_id + '?auth_token=' + token, function (error, response, body) {
   // request('http://wendycan-local.com:3003/api/v1/user?auth_token=' + token, function (error, response, body) { //Rails use 3003!!!
     // to save to db
     if (JSON.parse(body).username) {
@@ -33,26 +35,6 @@ io.of('/annotations').on('connection', function(socket){
   var cookie_string = socket.request.headers.cookie;
   var sid = cookie_string.split('=')[1];
   var user = {sid: sid};
-
-  socket.on('join chat', function (msg) {
-    console.log('annotations connected');
-
-    var index = usernames.indexOf(msg);
-    if (index < 0) {
-      user.name = msg;
-      users.push(user);
-      usernames.push(msg);
-    } else {
-      usernames[index].sid = sid;
-    }
-
-    var data = {
-      currentUser: msg,
-      users: usernames
-    };
-
-    io.of('/annotations').emit('join message', JSON.stringify(data));
-  });
 
   socket.on('change annotation', function(msg){
     var address = socket.handshake.address;
@@ -70,40 +52,9 @@ io.of('/annotations').on('connection', function(socket){
     io.of('/annotations').emit('chart message', JSON.stringify(data));
   });
 
-  socket.on('disconnect', function (data) {
-    console.log('message: leave' );
-
-    var msg;
-    for(var i in users) {
-      if (users[i].sid == sid) {
-        msg = users[i].name;
-        users.splice(i, 1);
-        usernames.splice(i, 1);
-      };
-    }
-
-    io.of('/annotations').emit('leave message', msg);
-  });
-
-  socket.on('leave page', function () {
-    console.log('message: leave' );
-
-    var msg;
-    for(var i in users) {
-      if (users[i].sid == sid) {
-        msg = users[i].name;
-        users.splice(i, 1);
-        usernames.splice(i, 1);
-      };
-    }
-
-    io.of('/annotations').emit('leave message', msg);
-  });
-
 });
 http.listen(3003, function(){
 console.log('listening on *:3003');
 });
-
 
 module.exports = router;
